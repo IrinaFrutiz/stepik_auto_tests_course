13dd8cc9ffaeb179af60d7be226de34444df3576
irina <irinafrutiz@gmail.com>
2023-04-14 16:38:44 +0200

    change 1_6_10 add requirement.txt

--- lesson1_6_10.py
+++ lesson1_6_10.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def ckecksite(expression):
-    try:
+
+try:
         link = "http://suninjuly.github.io/registration2.html"
         browser = webdriver.Chrome()
         browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
+            # Ваш код, который заполняет обязательные поля
         input_firstname = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[1]/input")
         input_firstname.send_keys("First name")
         input_lastname = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[2]/input")
@@ -16,23 +16,23 @@ def ckecksite(expression):
         input_email = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[3]/input")
         input_email.send_keys("e-mail")
 
-    # Отправляем заполненную форму
+             # Отправляем заполненную форму
         button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
         time.sleep(1)
 
-    # находим элемент, содержащий текст
+        # находим элемент, содержащий текст
         welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         assert "Congratulations! You have successfully registered!" == welcome_text
 
-    finally:
+finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
         time.sleep(10)
     # закрываем браузер после всех манипуляций
