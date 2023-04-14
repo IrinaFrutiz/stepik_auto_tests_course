c92cba885efd478bbc260e4c3aec3ac151fe5cb1
irina <irinafrutiz@gmail.com>
2023-04-14 12:30:25 +0200

    add new file

---
+++ 3_2_12.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestClass(unittest.TestCase):
+    def testclass1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+
+    def testclass2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
