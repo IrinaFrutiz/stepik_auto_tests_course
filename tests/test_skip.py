d0d1137bfd53093be37dbf56a9c1eb0b9a4f19ac
irina <irinafrutiz@gmail.com>
2023-04-19 12:57:07 +0200

    mark @pytest.mark.xfail ожидаем что упадёт 3.5.5

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -23,3 +23,9 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
+
